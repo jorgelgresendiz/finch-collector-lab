7c5a45416f946c91b2bc343ded57ac5e3a341495
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2020-01-02 11:48:17 -0600

    add feeding form

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Finch
+from .models import Finch, Feeding
 
 # Register your models here.
 admin.site.register(Finch)
+
+# register feeding model here
+admin.site.register(Feeding)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from .models import Finch, Feeding
+from .forms import FeedingForm
 
 # Create your views here.
 
@@ -22,7 +23,13 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+    # instantitate feeding form to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        # include the finch and feeding form in the context
+        'finch': finch,
+        'feeding_form': feeding_form
+    })
 
 
 class FinchCreate(CreateView):
